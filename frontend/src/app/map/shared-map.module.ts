07beeae54268cd26b1fb7398106b9b8e27b3eee2
Uwe Gaertner <uwe.gaertner@pergocon.de>
2023-05-24 17:24:47 +0200

    move DetailsLinkComponent

--- frontend/src/app/map/shared-map.module.ts
+++ frontend/src/app/map/shared-map.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { MapComponent } from './map/components/map/map.component';
-import { DetailsLinkComponent } from './map/components/map/details-link/details-link.component';
+import { DetailsLinkComponent } from './components/details-link/details-link.component';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
