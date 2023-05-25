97fda5e82e741051b2191342d264de75a2b9d506
Marco T <55094695+exxeta-mata@users.noreply.github.com>
2023-05-25 23:04:18 +0200

    feat(dan import): dan import part I

---
+++ backend/src/main/java/com/exxeta/wpgwn/wpgwnapp/dan/DanData.java
@@ -0,0 +1,18 @@
+package com.exxeta.wpgwn.wpgwnapp.dan;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Getter
+@Setter
+public class DanData {
+    private CampaignData campaign;
+//    private List<CampaignsData> campaigns;
+//    public CampaignsData getCampaign(){
+//        return campaigns.get(0);
+//    }
+}
