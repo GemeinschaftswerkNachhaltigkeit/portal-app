9bfba788f86b0f5bce60844fa8b6d5cbe079b66f
Patrick Hebner <patrick.hebner@exxeta.com>
2023-05-26 08:34:49 +0200

    fix: multiple small bugfixes
    - expired action label text
    - hint for dan action period from cms
    - url suffix icon position

--- frontend/src/app/shared/components/expired-badge/expired-badge.component.ts
+++ frontend/src/app/shared/components/expired-badge/expired-badge.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-expired-badge',
   templateUrl: './expired-badge.component.html',
   styleUrls: ['./expired-badge.component.scss']
 })
-export class ExpiredBadgeComponent {}
+export class ExpiredBadgeComponent {
+  @Input() dan = false;
+}
