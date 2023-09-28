78c5d856bf75c75b34712151f599c6227b839e0e
Yue Chen <yue.chen@exxeta.com>
2023-09-28 13:03:54 +0200

    feat: ExpiredValidator Dependent on Configuration Setting

--- backend/src/main/java/com/exxeta/wpgwn/wpgwnapp/dan_import/validator/CampaignExpiredValidator.java
+++ backend/src/main/java/com/exxeta/wpgwn/wpgwnapp/dan_import/validator/CampaignExpiredValidator.java
@@ -1,31 +1,50 @@
 package com.exxeta.wpgwn.wpgwnapp.dan_import.validator;
 
-import java.time.Instant;
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
-
+import com.exxeta.wpgwn.wpgwnapp.activity.DanRangeService;
 import com.exxeta.wpgwn.wpgwnapp.dan_import.exception.DanXmlImportIgnoredException;
 import com.exxeta.wpgwn.wpgwnapp.dan_import.xml.Campaign;
 
 import com.google.common.collect.Maps;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static com.exxeta.wpgwn.wpgwnapp.WpgwnAppApplication.DEFAULT_ZONE_ID;
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 /**
  * CampaignExpiredValidator : Check if a Dan has expired.
  */
 @Component
+@RequiredArgsConstructor
 public class CampaignExpiredValidator {
 
+    private final Clock clock;
 
-    public void validate(Campaign campaign, Instant now) {
+    private final DanRangeService danRangeService;
+
+    public void validate(Campaign campaign) {
 
         final Map<String, String> errorMessages = Maps.newConcurrentMap();
 
-        if (nonNull(campaign.getDateStart()) && nonNull(campaign.getDateEnd())
-                && campaign.getDateEnd().isBefore(now)) {
-            errorMessages.put("date_end", "validation.campaign.is.expired");
+        LocalDateTime startDan = danRangeService.getDanSetting().getStartDan();
+
+        if (nonNull(campaign.getDateStart()) && nonNull(campaign.getDateEnd())) {
+
+            if (nonNull(startDan) && campaign.getDateStart().atZone(DEFAULT_ZONE_ID)
+                    .toLocalDateTime().isBefore(startDan)) {
+                errorMessages.put("date_start", "validation.campaign.is.expired");
+
+            } else if (isNull(startDan) && campaign.getDateEnd().isBefore(Instant.now(clock))) {
+                errorMessages.put("date_end", "validation.campaign.is.expired");
+            }
         }
 
         if (!errorMessages.isEmpty()) {
--- backend/src/test/java/com/exxeta/wpgwn/wpgwnapp/dan_import/validator/CampaignExpiredValidatorTest.java
+++ backend/src/test/java/com/exxeta/wpgwn/wpgwnapp/dan_import/validator/CampaignExpiredValidatorTest.java
@@ -1,34 +1,71 @@
 package com.exxeta.wpgwn.wpgwnapp.dan_import.validator;
 
-import java.time.Instant;
-import java.util.Map;
-
-import org.junit.jupiter.api.Test;
-
+import com.exxeta.wpgwn.wpgwnapp.activity.DanRangeService;
+import com.exxeta.wpgwn.wpgwnapp.activity.dto.DanSetting;
 import com.exxeta.wpgwn.wpgwnapp.dan_import.exception.DanXmlImportIgnoredException;
 import com.exxeta.wpgwn.wpgwnapp.dan_import.xml.Campaign;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CampaignExpiredValidatorTest {
 
+    private Clock clock = Clock.fixed(Instant.parse("2023-10-08T00:00:00.000Z"), ZoneId.of("Europe/Berlin"));
+
+    private DanRangeService danRangeService = mock(DanRangeService.class);
+
+    private DanSetting danSetting = new DanSetting(true, true,
+            LocalDateTime.of(2023, 6, 10, 0, 0, 0, 0),
+            LocalDateTime.of(2023, 10, 10, 0, 0, 0, 0),
+            LocalDateTime.of(2023, 6, 10, 0, 0, 0, 0),
+            LocalDateTime.of(2023, 10, 10, 0, 0, 0, 0) );
+
     @Test
-    public void testValidate_WhenCampaignExpired_ThrowsException() {
+    public void testValidate_WhenCampaignStartExpired_ThrowsException() {
         // Create test data
         Campaign campaign = new Campaign();
         campaign.setDateStart(Instant.parse("2023-06-01T00:00:00Z"));
         campaign.setDateEnd(Instant.parse("2023-06-10T00:00:00Z"));
-        Instant now = Instant.parse("2023-06-15T00:00:00Z");
 
+        when(danRangeService.getDanSetting()).thenReturn(danSetting);
         // Create the object under test
-        CampaignExpiredValidator validator = new CampaignExpiredValidator();
+        CampaignExpiredValidator validator = new CampaignExpiredValidator(clock, danRangeService);
 
         // Assert that the exception is thrown
         DanXmlImportIgnoredException exception = assertThrows(DanXmlImportIgnoredException.class,
-                () -> validator.validate(campaign, now));
+                () -> validator.validate(campaign));
+
+        // Assert the error message
+        Map<String, String> errorMessages = exception.getErrorMessages();
+        assertTrue(errorMessages.containsKey("date_start"));
+        assertEquals("validation.campaign.is.expired", errorMessages.get("date_start"));
+    }
+
+    @Test
+    public void testValidate_WhenCampaignEndExpired_ThrowsException() {
+        // Create test data
+        Campaign campaign = new Campaign();
+        campaign.setDateStart(Instant.parse("2023-06-01T00:00:00Z"));
+        campaign.setDateEnd(Instant.parse("2023-06-10T00:00:00Z"));
+
+        DanSetting danSetting = new DanSetting(true, true, null, null, null, null);
+
+        when(danRangeService.getDanSetting()).thenReturn(danSetting);
+        // Create the object under test
+        CampaignExpiredValidator validator = new CampaignExpiredValidator(clock, danRangeService);
+
+        // Assert that the exception is thrown
+        DanXmlImportIgnoredException exception = assertThrows(DanXmlImportIgnoredException.class,
+                () -> validator.validate(campaign));
 
         // Assert the error message
         Map<String, String> errorMessages = exception.getErrorMessages();
@@ -40,14 +77,15 @@ public class CampaignExpiredValidatorTest {
     public void testValidate_WhenCampaignNotExpired_NoExceptionThrown() {
         // Create test data
         Campaign campaign = new Campaign();
-        campaign.setDateStart(Instant.parse("2023-06-01T00:00:00Z"));
+        campaign.setDateStart(Instant.parse("2023-06-11T00:00:00Z"));
         campaign.setDateEnd(Instant.parse("2023-06-20T00:00:00Z"));
-        Instant now = Instant.parse("2023-06-15T00:00:00Z");
 
         // Create the object under test
-        CampaignExpiredValidator validator = new CampaignExpiredValidator();
+        when(danRangeService.getDanSetting()).thenReturn(danSetting);
+        // Create the object under test
+        CampaignExpiredValidator validator = new CampaignExpiredValidator(clock, danRangeService);
 
         // Call the method and assert no exception is thrown
-        assertDoesNotThrow(() -> validator.validate(campaign, now));
+        assertDoesNotThrow(() -> validator.validate(campaign));
     }
 }
