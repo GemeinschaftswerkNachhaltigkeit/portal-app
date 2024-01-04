bd05e1c5bac66763648533b232da394afc9362ee
Yue Chen <yue.chen@exxeta.com>
2024-01-04 12:48:38 +0100

    Organization import with multi-template support

--- backend/src/main/java/com/exxeta/wpgwn/wpgwnapp/excel_import/domain/ImportSource.java
+++ backend/src/main/java/com/exxeta/wpgwn/wpgwnapp/excel_import/domain/ImportSource.java
@@ -1,15 +1,22 @@
 package com.exxeta.wpgwn.wpgwnapp.excel_import.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ImportSource {
-    NONE,
-    RNE,
-    BERTELSMANN,
-    KVM,
+    NONE(""),
+    RNE("email/organisation-work-in-progress/privacy-consent/organisation-privacy-consent.html"),
+    BERTELSMANN(""),
+    KVM(""),
 
-    DNP,
+    DNP("email/organisation-work-in-progress/privacy-consent/organisation-privacy-consent-dnp.html"),
 
-    NRW,
+    NRW("email/organisation-work-in-progress/privacy-consent/organisation-privacy-consent-nrw.html"),
 
-    DAN_XML,
-    GLOBAL_GOALS_DIRECTORY
+    DAN_XML(""),
+    GLOBAL_GOALS_DIRECTORY("");
+
+    @Getter
+    private final String privacyConsentEmailTemplate;
 }
