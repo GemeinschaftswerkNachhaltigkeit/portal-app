bb6c62be96e76e0528840d1eaec93a8f28ed887d
Yue Chen <Yue.Chen@exxeta.com>
2024-01-18 18:19:30 +0100

    fix: chage source type in activity and activity_work_in_progress

--- backend/src/main/resources/db/migration/V0058__fix_source_column.sql
+++ backend/src/main/resources/db/migration/V0058__fix_source_column.sql
@@ -1,4 +1,12 @@
+DO $$
+BEGIN
+
+    IF EXISTS (SELECT 1
+               FROM information_schema.columns
+               WHERE table_name = 'activity' AND column_name = 'source' AND data_type = 'integer') THEN
+
 ALTER TABLE activity ADD COLUMN source_temp VARCHAR(255);
+
 UPDATE activity
 SET source_temp = CASE
                       WHEN source = 0 THEN 'LANDING_PAGE'
@@ -7,10 +15,27 @@ SET source_temp = CASE
                       WHEN source = 3 THEN 'IMPORT'
                       ELSE NULL
     END;
+
+
 ALTER TABLE activity DROP COLUMN source;
+
 ALTER TABLE activity RENAME COLUMN source_temp TO source;
 
+ELSE
+        RAISE NOTICE 'Column source does not exist or is not of type int4 in activity.';
+END IF;
+END $$;
+
+
+DO $$
+BEGIN
+
+    IF EXISTS (SELECT 1
+               FROM information_schema.columns
+               WHERE table_name = 'activity_work_in_progress' AND column_name = 'source' AND data_type = 'integer') THEN
+
 ALTER TABLE activity_work_in_progress ADD COLUMN source_temp VARCHAR(255);
+
 UPDATE activity_work_in_progress
 SET source_temp = CASE
                       WHEN source = 0 THEN 'LANDING_PAGE'
@@ -19,5 +44,13 @@ SET source_temp = CASE
                       WHEN source = 3 THEN 'IMPORT'
                       ELSE NULL
     END;
+
+
 ALTER TABLE activity_work_in_progress DROP COLUMN source;
+
 ALTER TABLE activity_work_in_progress RENAME COLUMN source_temp TO source;
+
+ELSE
+        RAISE NOTICE 'Column source does not exist or is not of type int4 in activity_work_in_progress.';
+END IF;
+END $$;
