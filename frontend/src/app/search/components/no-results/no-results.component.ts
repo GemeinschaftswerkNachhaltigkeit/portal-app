a6ed8f802f6579f7b7da7ef8056b512ca4138b52
Hebner <patrick@MN-EX7466.local>
2024-01-19 09:53:13 +0100

    feat: add link and new text for no results

--- frontend/src/app/search/components/no-results/no-results.component.ts
+++ frontend/src/app/search/components/no-results/no-results.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AllResultsLinkComponent } from '../all-results-link/all-results-link.component';
 
 @Component({
   selector: 'app-no-results',
   standalone: true,
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, AllResultsLinkComponent],
   templateUrl: './no-results.component.html',
   styleUrl: './no-results.component.scss'
 })
-export class NoResultsComponent {}
+export class NoResultsComponent {
+  @Input() resultType: string;
+  @Input() searchTerm: string;
+}
